1ddde232a6e6522fea0ae170f7836490d2edbfaa
Juswan <email.anda@gmail.com>
2026-09-10 21:23:54 +0800

    fix bug

--- desa_wawatu/desa/config/database.php
+++ desa_wawatu/desa/config/database.php
@@ -1,5 +1,27 @@
 <?php
-$db['default']['hostname'] = 'db';
-$db['default']['username'] = 'opensid';
-$db['default']['password'] = 'opensid123';
-$db['default']['database'] = 'opensid_desa_wawatu';
+defined('BASEPATH') || exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = true;
+
+$db['default'] = [
+    'dsn'   => '',
+    'hostname' => 'db-wawatu',
+    'username' => 'opensid',
+    'password' => 'opensid123',
+    'database' => 'opensid',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => array(),
+    'save_queries' => true
+];
