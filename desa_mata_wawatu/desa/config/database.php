503233e46754403b9db258058f563d7fc637338a
Juswan <email.anda@gmail.com>
2026-09-10 20:49:54 +0800

    Fix koneksi database (menambahkan host, user, dan password)

--- desa_mata_wawatu/desa/config/database.php
+++ desa_mata_wawatu/desa/config/database.php
@@ -1,2 +1,5 @@
 <?php
+$db['default']['hostname'] = 'db';
+$db['default']['username'] = 'opensid';
+$db['default']['password'] = 'opensid123';
 $db['default']['database'] = 'opensid_desa_mata_wawatu';
